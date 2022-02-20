960d3a880702c4a8a81dcbe789c80ac20aca2f35
binarygit <inscription4@outlook.com>
2022-02-20 17:19:05 +0545

    event requires a body to be saved

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
   validates :name, uniqueness: true, presence: true
+  validates :body, presence: true
   validates :date, presence: true
   validate :invalid_date
 
