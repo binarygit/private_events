afb50993d3aeea473a0e940ee29e149232a3e2a9
binarygit <inscription4@outlook.com>
2022-02-20 12:52:25 +0545

    create association of event and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
+
   validates :email, presence: true, uniqueness: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
