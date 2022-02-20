6e5e21d6d581a3f3c503cd19fa8843056c27ef46
binarygit <inscription4@outlook.com>
2022-02-20 10:35:44 +0545

    create sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash.notice = 'You are successfully logged in'
+      redirect_to root_path
+    else
+      flash.now.alert = 'Sorry Something went wrong'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash.notice = 'Successfully Logged Out'
+    redirect_to root_path
+  end
 end
