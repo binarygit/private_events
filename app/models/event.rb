5198064c610465478a731a468433e023cf35d4a1
binarygit <inscription4@outlook.com>
2022-02-20 14:17:20 +0545

    add validations for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,17 @@
 class Event < ApplicationRecord
+  include Comparable
   belongs_to :creator, class_name: 'User'
 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
+  validates :date, presence: true
+  validate :invalid_date
+
+  private
+
+  def invalid_date
+    return if date.nil?
+    unless date > Date.yesterday
+      errors.add(:date, 'can\'t be in the past, stupid')
+    end
+  end
 end
