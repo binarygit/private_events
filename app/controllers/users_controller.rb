3c62214c975ebbdd15ab70a03b54fb08d87455d8
binarygit <inscription4@outlook.com>
2022-02-20 10:02:01 +0545

    add validations to user, users can signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(form_params)
+    if @user.save
+      flash.notice = 'User successfully created'
+      redirect_to root_path
+    else
+      flash.now.alert = 'User could not be created'
+      render :new
+    end
+  end
+
+  private
+
+  def form_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
