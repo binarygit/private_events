817a336c7c459c21a5023958422eb4401d79f904
binarygit <inscription4@outlook.com>
2022-02-21 07:04:04 +0545

    debug and make it work

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   
   has_many :event_attendances
-  has_many :attendees, through: :event_attendances, source: :user
+  has_many :attendees, through: :event_attendances, source: :attendee
 
   validates :name, uniqueness: true, presence: true
   validates :date, presence: true
--- app/models/event_attendance.rb
+++ app/models/event_attendance.rb
@@ -1,4 +1,4 @@
 class EventAttendance < ApplicationRecord
-  belongs_to :user
+  belongs_to :attendee, class_name: 'User'
   belongs_to :event
 end
